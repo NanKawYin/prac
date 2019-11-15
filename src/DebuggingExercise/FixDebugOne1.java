05bfbb83942405adec9ef96ba7f98c2be1863b31
NanKawYin <kawyin.nan@my.jcu.edu.au>
2019-11-15 11:50:52 +0800

    "Fix Debug"

---
+++ src/DebuggingExercise/FixDebugOne1.java
@@ -0,0 +1,10 @@
+package DebuggingExercise;
+
+public class FixDebugOne1 {
+
+   /* This program displays a greeting */
+   public static void main(String[] args)
+   {
+      System.out.print("Hello");
+   }
+}
---
+++ src/DebuggingExercise/FixDebugOne4.java
@@ -0,0 +1,10 @@
+package DebuggingExercise;
+
+import javax.swing.JOptionPane;
+public class FixDebugOne4
+{
+  public static void main(String[] args)
+  {
+     JOptionPane.showMessageDialog(null, "First GUI program");
+  }
+}
